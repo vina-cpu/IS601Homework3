bc86675d430ef5eaef97df1998c4e2e297d3f935
vina-cpu <victorianawalany@gmail.com>
2025-02-09 15:20:19 -0700

    fixing issue for now will come back to this

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,6 +1,14 @@
+from typing import Callable
 from calculator.operation import Operation
 from calculator.history import History
 from calculator.calculation import Calculation
+
+#class Calculator:
+    #@staticmethod
+    #def newCalculation(a: float, b: float, oper Callable[[float, float], float]) -> float:
+        #newCalc = Calculation.create(a, b, oper)
+        #History.add_calculation(newCalc)
+        #return newCalc.perform()
 
     
     
