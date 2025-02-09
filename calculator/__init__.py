65304f31fbe603882c932221f3f9a85f163fd720
vina-cpu <victorianawalany@gmail.com>
2025-02-09 16:32:56 -0700

    adding Calculator class and merging history tracking with calculation operations

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -3,13 +3,27 @@ from calculator.operation import Operation
 from calculator.history import History
 from calculator.calculation import Calculation
 
-#class Calculator:
-    #@staticmethod
-    #def newCalculation(a: float, b: float, oper Callable[[float, float], float]) -> float:
-        #newCalc = Calculation.create(a, b, oper)
-        #History.add_calculation(newCalc)
-        #return newCalc.perform()
-
+class Calculator:
     
+    #merging the calculations with the history, so whenever you call this it does the calculation and adds it to the history too
+    @staticmethod
+    def newCalculation(a: float, b: float, oper: Callable[[float, float], float]) -> float:
+        newCalc = Calculation.create(a, b, oper)
+        History.add_calculation(newCalc)
+        return newCalc.perform()
     
+    @staticmethod
+    def add(a: float, b: float):
+        return Calculator.newCalculation(a, b, Operation.add)
     
+    @staticmethod
+    def subtract(a: float, b: float) -> float:
+        return Calculator.newCalculation(a, b, Operation.subtract)
+    
+    @staticmethod
+    def multiply(a: float, b: float) -> float:
+        return Calculator.newCalculation(a, b, Operation.multiply)
+    
+    @staticmethod
+    def divide(a: float, b: float) -> float:
+        return Calculator.newCalculation(a, b, Operation.divide)
