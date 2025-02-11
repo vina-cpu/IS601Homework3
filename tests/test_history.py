f4b4396c0944232dbca37a815c7619c8900eeaa6
vina-cpu <victorianawalany@gmail.com>
2025-02-10 19:23:40 -0700

    making tests for Calculator class more complete

--- tests/test_history.py
+++ tests/test_history.py
@@ -90,19 +90,29 @@ def test_calculator_redo_last_a(setup_history):
 def test_calculator_redo_last_b(setup_history):
     '''test for changing the last calculation's b and making a new calculation'''
     assert Calculator.redoLastCalcB(1) == 5
+    assert len(History.get_history()) == 3
+    assert History.get_index_calc(1).getB() == 4
 
 def test_calculator_redo_last_operation(setup_history):
     '''test for changing the last calculation's operation and making a new calculation'''
     assert Calculator.redoLastCalcOperation(Operation.add) == 9
+    assert len(History.get_history()) == 3
+    assert History.get_index_calc(1).getOperation().__name__ == Operation.multiply.__name__
 
 def test_calculator_redo_a(setup_history):
     '''test for changing a calculation's a and making a new calculation'''
     assert Calculator.redoCalcA(History.get_last_calc(), 3) == 12
+    assert len(History.get_history()) == 3
+    assert History.get_index_calc(1).getA() == 5
 
 def test_calculator_redo_b(setup_history):
     '''test for changing a calculation's b and making a new calculation'''
-    assert Calculator.redoCalcB(Calculation(1,2,Operation.add), 4) == 5
+    assert Calculator.redoCalcB(History.get_index_calc(0), 4) == 6
+    assert len(History.get_history()) == 3
+    assert History.get_index_calc(0).getB() == 3
 
 def test_calculator_redo_operation(setup_history):
     '''test for changing a calculation's operation and making a new calculation'''
     assert Calculator.redoCalcOperation(History.get_index_calc(0), Operation.subtract) == -1
+    assert len(History.get_history()) == 3
+    assert History.get_index_calc(0).getOperation().__name__ == Operation.add.__name__
