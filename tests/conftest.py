af10a250f461210a539ed5d5bb95dd42686531c7
vina-cpu <victorianawalany@gmail.com>
2025-02-16 12:50:39 -0700

    adding a new file specifically for the conftest.py, and adding pytest.addoption b/c i know what that does

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,3 +2,6 @@ import pytest
 from typing import Callable, List
 from faker import Faker
 from calculator.operation import Operation
+
+def pytest_addoption(parser):
+    parser.addoption("--num_records", action = "store", default = 10, type = int, help = "Number of test records to generate")
