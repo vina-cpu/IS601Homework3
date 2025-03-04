b7b7d5126f986578ef9143b2ab9409c8c20b33a0
vina-cpu <victorianawalany@gmail.com>
2025-03-03 21:21:59 -0700

    organizing so i have command and commandhandler in one place and then i can build logging on interface

--- command.py
+++ command.py
@@ -3,95 +3,31 @@
 #from decimal import Decimal, InvalidOperation
 #from calculator import Calculator
 #abc is abstract base class which allows me to use this pattern thingy 
+import importlib
+import pkgutil
+import plugins
 
-#class Command(ABC):
-#    @abstractmethod
-#    def execute(self):
-#        pass
-#    
-#class AddCommand(Command):
-#    def execute(self):
-#        print("What is your first number:")
-#        num1str: str = input(">>> ").strip()
-#        print("What is your second number:")
-#        num2str: str = input(">>> ").strip()
-#        print("Calculating ... ")
-#        try:
-#            num1, num2 = map(Decimal, [num1str, num2str])
-#            result = Calculator.add(num1, num2)
-#            print(f"The result of {num1} add {num2} is equal to {result}")
-#        except InvalidOperation:
-#            print(f"Invalid number input: {num1str} or {num2str} is not a valid number.")        
-#        except ValueError:
-#            print("An error occured: Cannot divide by zero")
-#        except Exception as e: #line missed in cov - don't know how to test this
-#            print(f"An error occured: {e}") #line missed in cov - don't know how to test this    
-#
-#class SubtractCommand(Command):
-#    def execute(self):
-#        print("What is your first number:")
-#        num1str: str = input(">>> ").strip()
-#        print("What is your second number:")
-#        num2str: str = input(">>> ").strip()
-#        print("Calculating ... ")
-#        try:
-#            num1, num2 = map(Decimal, [num1str, num2str])
-#            result = Calculator.subtract(num1, num2)
-#            print(f"The result of {num1} subtract {num2} is equal to {result}")
-#        except InvalidOperation:
-#            print(f"Invalid number input: {num1str} or {num2str} is not a valid number.")        
-#        except ValueError:
-#            print("An error occured: Cannot divide by zero")
-#        except Exception as e: #line missed in cov - don't know how to test this
-#            print(f"An error occured: {e}") #line missed in cov - don't know how to test this     #
-#
-#class MultiplyCommand(Command):
-#    def execute(self):
-#        print("What is your first number:")
-#        num1str: str = input(">>> ").strip()
-#        print("What is your second number:")
-#        num2str: str = input(">>> ").strip()
-#        print("Calculating ... ")
-#        try:
-#            num1, num2 = map(Decimal, [num1str, num2str])
-#            result = Calculator.multiply(num1, num2)
-#            print(f"The result of {num1} multiply {num2} is equal to {result}")
-#        except InvalidOperation:
-#            print(f"Invalid number input: {num1str} or {num2str} is not a valid number.")        
-#        except ValueError:
-#            print("An error occured: Cannot divide by zero")
-#        except Exception as e: #line missed in cov - don't know how to test this
-#            print(f"An error occured: {e}") #line missed in cov - don't know how to test this    ##
-#
-#class DivideCommand(Command):
-#    def execute(self):
-#        print("What is your first number:")
-#        num1str: str = input(">>> ").strip()
-#        print("What is your second number:")
-#        num2str: str = input(">>> ").strip()
-#        print("Calculating ... ")
-#        try:
-#            num1, num2 = map(Decimal, [num1str, num2str])
-#            result = Calculator.divide(num1, num2)
-#            print(f"The result of {num1} divide {num2} is equal to {result}")
-#        except InvalidOperation:
-#            print(f"Invalid number input: {num1str} or {num2str} is not a valid number.")        
-#        except ValueError:
-#            print("An error occured: Cannot divide by zero")
-#        except Exception as e: #line missed in cov - don't know how to test this
-#            print(f"An error occured: {e}") #line missed in cov - don't know how to test this    ##
-#
-#class ExitCommand(Command):
-#    def execute(self):
-#        print("Goodbye!")
-#        sys.exit(1)#
-#
-#class MenuCommand(Command):
-#    def execute(self):
-#        print("Here is a list of commands:")
-#        print("Type 'add' to add two numbers")
-#        print("Type 'subtract' to subtract two numbers")
-#        print("Type 'multiply' to multiply two numbers")
-#        print("Type 'divide' to divide two numbers")
-#        print("Type 'exit' to exit")
-       
+class Command:
+    #@abstractmethod
+    def execute(self):
+        pass
+
+class CommandHandler():
+    def __init__(self):
+        self.commands = {}
+    
+    def regCommand(self, commandName: str, command: Command):
+        self.commands[commandName] = command
+    
+    def executeCommand(self, commandName: str):
+        try:
+            self.commands[commandName].execute()
+        except KeyError:
+            print(f"No such command: {commandName}")
+    
+    def loadCommands(self):
+        '''adds all of the plugins in plugins'''
+        for _, name, _ in pkgutil.iter_modules(plugins.__path__):
+            module = importlib.import_module(f"plugins.{name}.__init__")
+            if hasattr(module, "command"):
+                self.regCommand(name, module.command)
--- interface.py
+++ interface.py
@@ -1,13 +1,5 @@
-import importlib
-import pkgutil
-import plugins
-#from abc import ABC, abstractmethod turns out did not end up needing this with plugin architecture, just added a command line at the bottom of each command
 from calculator import Calculator
-#from command import Command, AddCommand, SubtractCommand, MultiplyCommand, DivideCommand, ExitCommand, MenuCommand
-class Command:
-    #@abstractmethod
-    def execute(self):
-        pass
+from command import Command, CommandHandler
 
 class Interface:
     def __init__(self):
@@ -19,37 +11,6 @@ class Interface:
     
     def start(self):
         print("Hello! Type calculator commands to utilize the calculator, type 'menu' for a full list of commands, or type 'exit' to exit!")
-        
-        #self.commandHandler.regCommand("add", AddCommand())
-        #self.commandHandler.regCommand("subtract", SubtractCommand())
-        #self.commandHandler.regCommand("multiply", MultiplyCommand())
-        #self.commandHandler.regCommand("divide", DivideCommand())
-        #self.commandHandler.regCommand("menu", MenuCommand())
-        #self.commandHandler.regCommand("exit", ExitCommand())
         self.commandHandler.loadCommands()
-        
         while True:
-            self.commandHandler.executeCommand(input(">>> ").strip())
-
-class CommandHandler():
-    def __init__(self):
-        self.commands = {}
-    
-    def regCommand(self, commandName: str, command: Command):
-        self.commands[commandName] = command
-    
-    def executeCommand(self, commandName: str):
-        try:
-            self.commands[commandName].execute()
-        except KeyError:
-            print(f"No such command: {commandName}")
-    
-    def loadCommands(self):
-        '''adds all of the plugins in plugins'''
-        for _, name, _ in pkgutil.iter_modules(plugins.__path__):
-            module = importlib.import_module(f"plugins.{name}.__init__")
-            if hasattr(module, "command"):
-                self.regCommand(name, module.command)
-
-        
-    
+            self.commandHandler.executeCommand(input(">>> ").strip())       
